244820dbc4f8cddd26f5ffc54409117d75aab126
NicholasJohansan <nicholasjohansan@gmail.com>
2022-02-17 18:07:13 +0800

    Add calculator display update functionality

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 
+let calculatorDisplayText = "";
+const calculatorDisplay = document.querySelector("#display");
+const calculatorClearButton = document.querySelector("#clear_button")
+const calculatorNumberButtons = document.querySelectorAll("button[data-number]");
+const calculatorOperatorButtons = document.querySelectorAll("button[data-operator]");
+
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
@@ -10,3 +16,29 @@ const operate = (a, operator, b) => ({
   "*": multiply(a, b),
   "/": divide(a, b)
 }[operator]);
+
+const updateDisplay = (text) => {
+  calculatorDisplayText = text;
+  calculatorDisplay.innerText = calculatorDisplayText;
+};
+
+const addToDisplay = (text) => {
+  updateDisplay(`${calculatorDisplayText}${text}`);
+};
+
+const clearDisplay = () => updateDisplay("");
+
+calculatorNumberButtons.forEach(button => {
+  button.addEventListener("click", function() {
+    addToDisplay(this.innerText);
+  });
+});
+
+calculatorOperatorButtons.forEach(button => {
+  button.addEventListener("click", function() {
+    addToDisplay(` ${this.innerText} `);
+  });
+});
+
+calculatorClearButton.addEventListener('click', clearDisplay);
+
